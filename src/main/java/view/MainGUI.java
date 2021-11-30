c00ce1cb595adab2094c64ae15be984e2c2e24d0
Pablo <pablolog42@gmail.com>
2021-11-30 20:32:10 -0300

    Mejora UI 1

--- src/main/java/view/MainGUI.java
+++ src/main/java/view/MainGUI.java
@@ -1,14 +1,29 @@
 package view;
 
+import aventurasdemarcoyluis.controller.GameController;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+
+// https://docs.oracle.com/javafx/2/ui_controls/text-field.htm
 
 public class MainGUI extends Application {
 
-    Button button;
+    private Stage window;
+    private Scene playerEnteringNameScene;
+    private Scene gameScene;
+
+    private GameController controller;
+    private Button submitNameButton;
+
+    private TextArea textOutputBox;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,18 +46,82 @@ public class MainGUI extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hola mundo");
 
-        button = new Button("aprietame");
+        controller = new GameController();
 
-        StackPane layout = new StackPane();
+        window = primaryStage;
+        window.setTitle("Las Flipantes Aventuras de Marco y Luis");
 
-        layout.getChildren().add(button);
+        /*
+        First stage: Get player's name
+         */
 
-        Scene scene = new Scene(layout,400,400);
-        primaryStage.setScene(scene);
+        submitNameButton = new Button("Set Name");
+        GridPane layout = new GridPane();
+        // Setea el encuadre de la app
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        layout.setVgap(5);
+        layout.setHgap(5);
+        //Defining the Name text field
+        final TextField nameInputField = new TextField();
+        nameInputField.setPromptText("Enter your first name.");
+        nameInputField.setPrefColumnCount(10);
+        //Defining the label
+        final Label welcomeMessage = new Label("Bienvenido! Por favor, indica tu nombre:");
+        GridPane.setConstraints(welcomeMessage,0,0);
+        layout.getChildren().add(welcomeMessage);
 
-        primaryStage.show();
+        // Se agrega el field que setea el nombre
+        GridPane.setConstraints(nameInputField, 0, 1);
+        layout.getChildren().add(nameInputField);
+        // Se agrega boton que setea nombre
+        GridPane.setConstraints(submitNameButton, 1, 1);
+        layout.getChildren().add(submitNameButton);
+        //Setting an action for the Submit button
+        // We handle the button event directly in an anonimus handler class.
+        submitNameButton.setOnAction(e -> {
+            controller.setPlayer(nameInputField.getText());
+            window.setScene(gameScene);
+        });
+        playerEnteringNameScene = new Scene(layout,350,70);
+
+
+        /*
+        Second stage: The actual Game
+         */
+
+        // Setup ////////////
+        GridPane gameSceneLayout = new GridPane();
+        // Setea el encuadre de la app
+        gameSceneLayout.setPadding(new Insets(10, 10, 10, 10));
+        gameSceneLayout.setVgap(5);
+        gameSceneLayout.setHgap(5);
+        //////////////
+
+
+        //Phase Label
+        final Label currentPhaseLabel = new Label("La fase actual de juego es: " + controller.getCurrentPhase().toString());
+        GridPane.setConstraints(currentPhaseLabel,2,0);
+        gameSceneLayout.getChildren().add(currentPhaseLabel);
+
+        // Player name Label
+        final Label playerNameLabel = new Label("Hola!, " + controller.getPlayer().getPlayerName());
+        GridPane.setConstraints(playerNameLabel,0,0);
+        gameSceneLayout.getChildren().add(playerNameLabel);
+
+
+        gameScene =  new Scene(gameSceneLayout, 1000,600);
+
+
+
+
+
+
+        /*
+        Startpoint for the UI
+         */
+        window.setScene(playerEnteringNameScene);
+        window.show();
 
     }
 }
